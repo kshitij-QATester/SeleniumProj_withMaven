84d85d05ddf4598df004f5f1128cde9d4da9ad96
Admin <Admin@DESKTOP-VG5MMS3>
2026-01-18 11:45:54 +0530

    created Login test

---
+++ src/main/java/seotest/LoginFormTest.java
@@ -0,0 +1,16 @@
+package seotest;
+
+public class LoginFormTest extends BaseReport {
+	
+	
+	
+	public void LoginFormAutomate()
+	{
+		et=ex.createTest("Login Form Test Results");
+	}
+
+	
+	
+	
+	
+}
